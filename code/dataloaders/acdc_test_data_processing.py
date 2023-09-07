e5aad0d3b64db6c4287b7478bb63e1b645805bee
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-09-07 15:45:43 -0700

    add test slice code

--- code/dataloaders/acdc_test_data_processing.py
+++ code/dataloaders/acdc_test_data_processing.py
@@ -36,6 +36,39 @@ for case in mask_path:
     image_itk = sitk.ReadImage(image_path)
     image = sitk.GetArrayFromImage(image_itk)
 
+    scribble_path = case.replace("_gt", "_scribble")
+    scribble_itk = sitk.ReadImage(scribble_path)
+    scribble = sitk.GetArrayFromImage(scribble_itk)
+
+    image = MedicalImageDeal(image, percent=0.99).valid_img
+    image = (image - image.min()) / (image.max() - image.min())
+    print(image.shape)
+    image = image.astype(np.float32)
+    item = case.split("/")[-1].split(".")[0].replace("_gt", "")
+    if image.shape != label.shape:
+        print("Error")
+    print(item)
+    for slice_ind in range(image.shape[0]):
+        f = h5py.File(
+            './wsl4mis_data/ACDC/ACDC_testing_slices/{}_slice_{}.h5'.format(item, slice_ind), 'w')
+        f.create_dataset(
+            'image', data=image[slice_ind], compression="gzip")
+        f.create_dataset('label', data=label[slice_ind], compression="gzip")
+        f.create_dataset(
+            'scribble', data=scribble[slice_ind], compression="gzip")
+        f.close()
+        slice_num += 1
+print("Converted all raw ACDC volumes to 2D slices")
+print("Total {} slices".format(slice_num))
+
+for case in mask_path:
+    label_itk = sitk.ReadImage(case)
+    label = sitk.GetArrayFromImage(label_itk)
+
+    image_path = case.replace("_gt", "")
+    image_itk = sitk.ReadImage(image_path)
+    image = sitk.GetArrayFromImage(image_itk)
+
     image = MedicalImageDeal(image, percent=0.99).valid_img
     image = (image - image.min()) / (image.max() - image.min())
     print(image.shape)
@@ -51,5 +84,5 @@ for case in mask_path:
     f.create_dataset('label', data=label, compression="gzip")
     f.close()
     slice_num += 1
-print("Converted all ACDC volumes to 2D slices")
+print("Converted all raw ACDC volumes to ACDC volumes")
 print("Total {} slices".format(slice_num))
