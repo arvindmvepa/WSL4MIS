c448cf094d60fc54f1cb4764bb8b79153994a424
Zukang Yang <zukangy@Landis-MBP-2.attlocal.net>
2023-09-22 12:15:37 -0700

    Rewrote datasete_s2l to adapt to al-seg

--- code/val_2D.py
+++ code/val_2D.py
@@ -15,7 +15,7 @@ def calculate_metric_percase(pred, gt):
         return 0, 0
 
 
-def test_single_volume(image, label, net, classes, patch_size=[256, 256], gpus="0"):
+def test_single_volume(image, label, net, classes, patch_size=[256, 256], gpus="cuda:0"):
     image, label = image.squeeze(0).cpu().detach(
     ).numpy(), label.squeeze(0).cpu().detach().numpy()
     if len(image.shape) == 3:
@@ -50,7 +50,7 @@ def test_single_volume(image, label, net, classes, patch_size=[256, 256], gpus="
     return metric_list
 
 
-def test_single_volume_ds(image, label, net, classes, patch_size=[256, 256]):
+def test_single_volume_ds(image, label, net, classes, patch_size=[256, 256], gpus="cuda:0"):
     image, label = image.squeeze(0).cpu().detach(
     ).numpy(), label.squeeze(0).cpu().detach().numpy()
     if len(image.shape) == 3:
@@ -61,7 +61,7 @@ def test_single_volume_ds(image, label, net, classes, patch_size=[256, 256]):
             slice = zoom(
                 slice, (patch_size[0] / x, patch_size[1] / y), order=0)
             input = torch.from_numpy(slice).unsqueeze(
-                0).unsqueeze(0).float().cuda()
+                0).unsqueeze(0).float().to(gpus)
             net.eval()
             with torch.no_grad():
                 output_main, _, _, _ = net(input)
@@ -73,7 +73,7 @@ def test_single_volume_ds(image, label, net, classes, patch_size=[256, 256]):
                 prediction[ind] = pred
     else:
         input = torch.from_numpy(image).unsqueeze(
-            0).unsqueeze(0).float().cuda()
+            0).unsqueeze(0).float().to(gpus)
         net.eval()
         with torch.no_grad():
             output_main, _, _, _ = net(input)
